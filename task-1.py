f0c40bf1b0f77da0d927a2a618193041bff5e629
saman-re <saman.rezapoor1379@yahoo.com>
2025-03-01 01:02:12 +0330

    second commit

--- task-1.py
+++ task-1.py
@@ -1,11 +1,15 @@
 import json
+from confluent_kafka import Producer
 
-from kafka import KafkaProducer
+p = Producer({'bootstrap.servers': 'localhost'})
 
-producer = KafkaProducer(
-    bootstrap_servers='localhost:9092',
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
-)
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
 data = [
     {"id": "123", "full_name": "Ali", "timestamp": "2025/01/31 12:34:56", "active": "true"},
@@ -14,4 +18,14 @@ data = [
 ]
 
 for d in data:
-    producer.send('test_pipeline', d)
+    # Trigger any available delivery report callbacks from previous produce() calls
+    p.poll(0)
+
+    # Asynchronously produce a message. The delivery report callback will
+    # be triggered from the call to poll() above, or flush() below, when the
+    # message has been successfully delivered or failed permanently.
+    p.produce('mytopic', json.dumps(d).encode('utf-8'), callback=delivery_report)
+
+# Wait for any outstanding messages to be delivered and delivery report
+# callbacks to be triggered.
+p.flush()
