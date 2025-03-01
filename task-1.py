fb0b200e6d7b655e639442c84bdf886753471615
saman-re <saman.rezapoor1379@yahoo.com>
2025-03-01 22:06:51 +0330

    change kafka producer config

--- task-1.py
+++ task-1.py
@@ -1,7 +1,7 @@
 import json
 from confluent_kafka import Producer
 
-p = Producer({'bootstrap.servers': 'localhost'})
+p = Producer({'bootstrap.servers': 'kafka'})
 
 def delivery_report(err, msg):
     """ Called once for each message produced to indicate delivery result.
@@ -24,7 +24,7 @@ for d in data:
     # Asynchronously produce a message. The delivery report callback will
     # be triggered from the call to poll() above, or flush() below, when the
     # message has been successfully delivered or failed permanently.
-    p.produce('mytopic', json.dumps(d).encode('utf-8'), callback=delivery_report)
+    p.produce('test_pipeline', json.dumps(d).encode('utf-8'), callback=delivery_report)
 
 # Wait for any outstanding messages to be delivered and delivery report
 # callbacks to be triggered.
