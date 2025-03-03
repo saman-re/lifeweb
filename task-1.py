1031bc6d25ae23c19200644608755ecc2de70b6d
saman-re <saman.rezapoor1379@yahoo.com>
2025-03-03 05:36:19 +0330

    change task-1.py file to read from json file

--- task-1.py
+++ task-1.py
@@ -11,11 +11,11 @@ def delivery_report(err, msg):
     else:
         print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
-data = [
-    {"id": "123", "full_name": "Ali", "timestamp": "2025/01/31 12:34:56", "active": "true"},
-    {"id": "124", "full_name": " ", "timestamp": "2025-01-31T14:20:00Z", "active": "false",
-     "extra_field": "i should be removed!"}
-]
+def read_json_file(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+data = read_json_file('sample-data.json')
 
 time.sleep(60)
 
