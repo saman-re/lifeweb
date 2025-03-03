a0e4e5fdd2189a44663416c02181066e06ddcdef
saman-re <saman.rezapoor1379@yahoo.com>
2025-03-03 05:26:19 +0330

    add sleep inorder to logstash become ready

--- task-1.py
+++ task-1.py
@@ -1,11 +1,11 @@
 import json
+import time
+
 from confluent_kafka import Producer
 
 p = Producer({'bootstrap.servers': 'kafka'})
 
 def delivery_report(err, msg):
-    """ Called once for each message produced to indicate delivery result.
-        Triggered by poll() or flush(). """
     if err is not None:
         print('Message delivery failed: {}'.format(err))
     else:
@@ -17,15 +17,11 @@ data = [
      "extra_field": "i should be removed!"}
 ]
 
+time.sleep(60)
+
 for d in data:
-    # Trigger any available delivery report callbacks from previous produce() calls
     p.poll(0)
 
-    # Asynchronously produce a message. The delivery report callback will
-    # be triggered from the call to poll() above, or flush() below, when the
-    # message has been successfully delivered or failed permanently.
     p.produce('test_pipeline', json.dumps(d).encode('utf-8'), callback=delivery_report)
 
-# Wait for any outstanding messages to be delivered and delivery report
-# callbacks to be triggered.
 p.flush()
